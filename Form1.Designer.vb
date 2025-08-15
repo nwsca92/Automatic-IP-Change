bd8731e08bc87f1f1441da3f264462e1a3d65e8a
Nanang Wudy SCA <103475677+nwsca92@users.noreply.github.com>
2025-08-15 16:02:22 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        GroupBox1 = New GroupBox()
+        PictureBox1 = New PictureBox()
+        ComboBox1 = New ComboBox()
+        GroupBox2 = New GroupBox()
+        Button3 = New Button()
+        Button2 = New Button()
+        Button1 = New Button()
+        TextBox4 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        StatusStrip1 = New StatusStrip()
+        ToolStripStatusLabel1 = New ToolStripStatusLabel()
+        ToolStripStatusLabel2 = New ToolStripStatusLabel()
+        ToolStripStatusLabel3 = New ToolStripStatusLabel()
+        Timer1 = New Timer(components)
+        GroupBox1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox2.SuspendLayout()
+        StatusStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(PictureBox1)
+        GroupBox1.Controls.Add(ComboBox1)
+        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(200, 201)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Interface"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.yupp_generated_image_697772_removebg_preview
+        PictureBox1.Location = New Point(6, 56)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(188, 139)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 2
+        PictureBox1.TabStop = False
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(6, 22)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(188, 23)
+        ComboBox1.TabIndex = 1
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(Button3)
+        GroupBox2.Controls.Add(Button2)
+        GroupBox2.Controls.Add(Button1)
+        GroupBox2.Controls.Add(TextBox4)
+        GroupBox2.Controls.Add(TextBox3)
+        GroupBox2.Controls.Add(TextBox1)
+        GroupBox2.Controls.Add(TextBox2)
+        GroupBox2.Location = New Point(290, 17)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(352, 196)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Properties"
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(128, 151)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(105, 39)
+        Button3.TabIndex = 3
+        Button3.Text = "Set DHCP"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(11, 151)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(111, 39)
+        Button2.TabIndex = 2
+        Button2.Text = "Set IP Statis"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.BackgroundImageLayout = ImageLayout.Zoom
+        Button1.Location = New Point(239, 151)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(107, 39)
+        Button1.TabIndex = 1
+        Button1.Text = "Simpan IP"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(11, 109)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(335, 23)
+        TextBox4.TabIndex = 0
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(11, 80)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(335, 23)
+        TextBox3.TabIndex = 0
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(11, 22)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(335, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(11, 51)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(335, 23)
+        TextBox2.TabIndex = 0
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.Items.AddRange(New ToolStripItem() {ToolStripStatusLabel1, ToolStripStatusLabel2, ToolStripStatusLabel3})
+        StatusStrip1.Location = New Point(0, 277)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(683, 22)
+        StatusStrip1.TabIndex = 2
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' ToolStripStatusLabel1
+        ' 
+        ToolStripStatusLabel1.AutoSize = False
+        ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        ToolStripStatusLabel1.Size = New Size(70, 17)
+        ToolStripStatusLabel1.Text = "Initializing..."
+        ToolStripStatusLabel1.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' ToolStripStatusLabel2
+        ' 
+        ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        ToolStripStatusLabel2.Size = New Size(468, 17)
+        ToolStripStatusLabel2.Spring = True
+        ToolStripStatusLabel2.Text = "(No adapter selected)"
+        ' 
+        ' ToolStripStatusLabel3
+        ' 
+        ToolStripStatusLabel3.Name = "ToolStripStatusLabel3"
+        ToolStripStatusLabel3.Size = New Size(99, 17)
+        ToolStripStatusLabel3.Text = "--/--/----  --:--:--"
+        ToolStripStatusLabel3.TextAlign = ContentAlignment.MiddleRight
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 2000
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoSizeMode = AutoSizeMode.GrowAndShrink
+        ClientSize = New Size(683, 299)
+        Controls.Add(StatusStrip1)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MaximumSize = New Size(1000, 1000)
+        Name = "Form1"
+        SizeGripStyle = SizeGripStyle.Hide
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Ganti IP Otomatis"
+        GroupBox1.ResumeLayout(False)
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        StatusStrip1.ResumeLayout(False)
+        StatusStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel3 As ToolStripStatusLabel
+    Friend WithEvents Timer1 As Timer
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,325 @@
+﻿Imports System.Net.NetworkInformation
+Imports System.Net.Sockets
+Imports System.Linq
+Imports System.Net
+Imports System.IO
+Imports System.Text.Json
+Imports System.Drawing
+
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ComboBox1.Items.Clear() ' Optional: clear existing items
+
+        Dim adapters As NetworkInterface() = NetworkInterface.GetAllNetworkInterfaces()
+
+        For Each adapter As NetworkInterface In adapters
+            ComboBox1.Items.Add(adapter.Name)
+        Next
+
+        ' Optional: select the first adapter by default
+        If ComboBox1.Items.Count > 0 Then
+            ComboBox1.SelectedIndex = 0
+        End If
+    End Sub
+
+    Private Sub SaveSettings(ip As String, mask As String, gateway As String, dnsList As List(Of String), adapterName As String)
+        Dim settings As New NetworkSettings With {
+        .InterfaceName = adapterName,
+        .IPAddress = ip,
+        .SubnetMask = mask,
+        .Gateway = gateway,
+        .DnsServers = dnsList,
+        .ApplyTime = DateTime.Now
+    }
+
+        Dim json As String = JsonSerializer.Serialize(settings, New JsonSerializerOptions With {.WriteIndented = True})
+        Dim filePath As String = Path.Combine(Application.StartupPath, "SavedNetwork.json")
+
+        Try
+            File.WriteAllText(filePath, json)
+        Catch ex As Exception
+            MessageBox.Show("Failed to save settings: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+
+    Private Sub LoadSavedSettings()
+        Dim filePath As String = Path.Combine(Application.StartupPath, "SavedNetwork.json")
+        If Not File.Exists(filePath) Then
+            ' No saved settings
+            Return
+        End If
+
+        Try
+            Dim json As String = File.ReadAllText(filePath)
+            Dim settings As NetworkSettings = JsonSerializer.Deserialize(Of NetworkSettings)(json)
+
+            ' Display or auto-fill saved values
+            TextBox1.Text = $"Last saved: {settings.IPAddress}/{settings.SubnetMask} via {settings.InterfaceName}"
+            ' You can use this info later to restore the IP programmatically
+
+        Catch ex As Exception
+            MessageBox.Show("Could not load saved settings: " & ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        DisplayAdapterInfo()
+    End Sub
+    Private Sub DisplayAdapterInfo()
+        TextBox1.Text = "IP Address: -"
+        TextBox2.Text = "Subnet Mask: -"
+        TextBox3.Text = "Gateway: -"
+        TextBox4.Text = "DNS: -"
+
+        Dim selectedItem As String = ComboBox1.SelectedItem?.ToString()
+        If String.IsNullOrEmpty(selectedItem) OrElse selectedItem = "Tidak ditemukan Wi-Fi adapter " Then
+            Return
+        End If
+
+        ' Get adapter info
+        Dim adapter As NetworkInterface = NetworkInterface.GetAllNetworkInterfaces() _
+            .FirstOrDefault(Function(nic) nic.Name = selectedItem AndAlso
+                            nic.NetworkInterfaceType = NetworkInterfaceType.Wireless80211)
+
+        If adapter Is Nothing OrElse adapter.OperationalStatus <> OperationalStatus.Up Then
+            TextBox1.Text = "IP Address: (Adapter tidak aktif)"
+            Return
+        End If
+
+        Dim ipProps As IPInterfaceProperties = adapter.GetIPProperties()
+
+        ' --- IPv4 Address & Subnet Mask ---
+        Dim unicastAddress As UnicastIPAddressInformation = ipProps.UnicastAddresses _
+            .FirstOrDefault(Function(addr) addr.Address.AddressFamily = AddressFamily.InterNetwork)
+
+        If unicastAddress IsNot Nothing Then
+            TextBox1.Text = "IP Address: " & unicastAddress.Address.ToString()
+            TextBox2.Text = "Subnet Mask: " & unicastAddress.IPv4Mask.ToString()
+        End If
+
+        ' --- Default Gateway ---
+        If ipProps.GatewayAddresses.Count > 0 Then
+            Dim gateway As GatewayIPAddressInformation = ipProps.GatewayAddresses(0)
+            TextBox3.Text = "Gateway: " & gateway.Address.ToString()
+        Else
+            TextBox3.Text = "Gateway: (none)"
+        End If
+
+        ' --- DNS Servers ---
+        Dim dnsList As List(Of String) = New List(Of String)
+        For Each dns As IPAddress In ipProps.DnsAddresses
+            If dns.AddressFamily = AddressFamily.InterNetwork Then ' IPv4 only
+                dnsList.Add(dns.ToString())
+            End If
+        Next
+
+        If dnsList.Count > 0 Then
+            TextBox4.Text = "DNS: " & String.Join(", ", dnsList)
+        Else
+            TextBox4.Text = "DNS: (automatic / not assigned)"
+        End If
+    End Sub
+    Public Class NetworkSettings
+        Public Property InterfaceName As String
+        Public Property IPAddress As String
+        Public Property SubnetMask As String
+        Public Property Gateway As String
+        Public Property DnsServers As List(Of String)
+        Public Property ApplyTime As Date ' Optional: when it was saved
+    End Class
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Get current values displayed (or fetch again from adapter)
+        Dim ip As String = TextBox1.Text.Replace("IP Address: ", "")
+        Dim mask As String = TextBox2.Text.Replace("Subnet Mask: ", "")
+        Dim gateway As String = TextBox3.Text.Replace("Gateway: ", "")
+        Dim dnsText As String = TextBox4.Text.Replace("DNS: ", "")
+        Dim dnsList As New List(Of String)
+
+        If dnsText.Contains(",") Then
+            dnsList.AddRange(dnsText.Split(",").Select(Function(s) s.Trim()))
+        ElseIf Not String.IsNullOrEmpty(dnsText) AndAlso Not dnsText.Contains("automatic") Then
+            dnsList.Add(dnsText)
+        End If
+
+        Dim selectedAdapter As String = ComboBox1.SelectedItem?.ToString()
+
+        If selectedAdapter = "Tidak ditemukan Wi-Fi adapter" Or String.IsNullOrEmpty(selectedAdapter) Then
+            MessageBox.Show("Pilih adapter yang sesuai.")
+            Return
+        End If
+
+        SaveSettings(ip, mask, gateway, dnsList, selectedAdapter)
+        MessageBox.Show("Pengaturan berhasil disimpan!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub ApplySavedSettings(settings As NetworkSettings)
+        If settings Is Nothing Then
+            MessageBox.Show("Parameter tidak ditemukan", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Confirm action
+        Dim result As DialogResult = MessageBox.Show(
+            $"Ganti IP statis untuk '{settings.InterfaceName}'?{vbCrLf}{vbCrLf}" &
+            $"IP: {settings.IPAddress}{vbCrLf}" &
+            $"Mask: {settings.SubnetMask}{vbCrLf}" &
+            $"Gateway: {settings.Gateway}{vbCrLf}" &
+            $"DNS: {String.Join(", ", settings.DnsServers)}",
+            "Apply Network Settings",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question)
+
+        If result = DialogResult.No Then Return
+
+        Try
+            ' 1. Set static IP and subnet
+            RunNetsh($"interface ip set address ""{settings.InterfaceName}"" static {settings.IPAddress} {settings.SubnetMask} {settings.Gateway}")
+
+            ' 2. Set primary DNS
+            If settings.DnsServers.Count > 0 Then
+                RunNetsh($"interface ip set dns ""{settings.InterfaceName}"" static {settings.DnsServers(0)}")
+
+                ' Set secondary/tertiary DNS as "add" (not "static")
+                For i As Integer = 1 To settings.DnsServers.Count - 1
+                    RunNetsh($"interface ip add dns ""{settings.InterfaceName}"" {settings.DnsServers(i)} index={i + 1}")
+                Next
+            End If
+
+            MessageBox.Show("Berhasil mengganti IP ke statis!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show("Gagal mengganti IP ke statis: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+    Private Sub RunNetsh(args As String)
+        Dim startInfo As New ProcessStartInfo With {
+            .FileName = "netsh",
+            .Arguments = args,
+            .UseShellExecute = False,
+            .RedirectStandardOutput = True,
+            .RedirectStandardError = True,
+            .CreateNoWindow = True,
+            .WindowStyle = ProcessWindowStyle.Hidden
+        }
+
+        Using p As Process = Process.Start(startInfo)
+            p.WaitForExit()
+            Dim output As String = p.StandardOutput.ReadToEnd()
+            Dim errorOut As String = p.StandardError.ReadToEnd()
+
+            If p.ExitCode <> 0 Then
+                Throw New Exception($"Exit Code {p.ExitCode}{If(String.IsNullOrEmpty(errorOut), "", "; Error: " & errorOut)}")
+            End If
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim filePath As String = Path.Combine(Application.StartupPath, "SavedNetwork.json")
+        If Not File.Exists(filePath) Then
+            MessageBox.Show("Parameter jaringan tidak ditemukan, coba simpan dulu", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Try
+            Dim json As String = File.ReadAllText(filePath)
+            Dim savedSettings As NetworkSettings = JsonSerializer.Deserialize(Of NetworkSettings)(json)
+            ApplySavedSettings(savedSettings)
+
+        Catch ex As Exception
+            MessageBox.Show("Tidak bisa mengembalikan parameter: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim selectedAdapter As String = ComboBox1.SelectedItem?.ToString()
+        If String.IsNullOrEmpty(selectedAdapter) OrElse selectedAdapter = "No Wi-Fi adapter found" Then
+            MessageBox.Show("Select a valid Wi-Fi adapter.")
+            Return
+        End If
+
+        Dim result As DialogResult = MessageBox.Show($"Ganti '{selectedAdapter}' ke otomatis (DHCP)?", "DHCP", MessageBoxButtons.YesNo)
+        If result = DialogResult.No Then Return
+
+        Try
+            RunNetsh($"interface ip set address ""{selectedAdapter}"" dhcp")
+            RunNetsh($"interface ip set dns ""{selectedAdapter}"" dhcp")
+            MessageBox.Show("Jaringan berganti ke DHCP.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show("Gagal mengganti ke DHCP: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        ' ✅ 1. Update Jam (Kanan)
+        ToolStripStatusLabel3.Text = DateTime.Now.ToString("dd-MMM-yyyy  HH:mm:ss")
+
+        ' ✅ 2. Cek Status Jaringan
+        If Not IsNetworkConnected() Then
+            ToolStripStatusLabel1.Text = "Status: No Network 🔴"
+            ToolStripStatusLabel1.ForeColor = Color.Red
+            ToolStripStatusLabel1.Text = "(No network)"
+        Else
+            If IsInternetAvailable() Then
+                ToolStripStatusLabel1.Text = "Status: Online 🌐"
+                ToolStripStatusLabel1.ForeColor = Color.Green
+            Else
+                ToolStripStatusLabel1.Text = "Status: Limited (No Internet) 🟡"
+                ToolStripStatusLabel1.ForeColor = Color.Orange
+            End If
+        End If
+        If ComboBox1.Items.Count > 0 AndAlso ComboBox1.SelectedItem IsNot Nothing Then
+            Dim ipText As String = TextBox1.Text
+            If ipText.Contains("192.") Or ipText.Contains("10.") Or ipText.Contains("172.") Then
+                Dim ip As String = ipText.Replace("IP Address: ", "")
+                ToolStripStatusLabel2.Text = $"IPv4: {ip}"
+            Else
+                ToolStripStatusLabel2.Text = "(IP not assigned)"
+            End If
+        Else
+            ToolStripStatusLabel2.Text = "(No adapter)"
+        End If
+    End Sub
+
+    Private Function IsNetworkConnected() As Boolean
+        Return NetworkInterface.GetIsNetworkAvailable()
+    End Function
+
+    ' Fungsi: Cek apakah bisa akses internet (ping Google DNS)
+    Private Function IsInternetAvailable() As Boolean
+        Try
+            Using ping As New Ping()
+                Dim reply = ping.Send("8.8.8.8", 1500) ' Timeout 1.5 detik
+                Return reply.Status = IPStatus.Success
+            End Using
+        Catch
+            Return False
+        End Try
+    End Function
+
+    ' Fungsi: Cek apakah gateway bisa dijangkau (opsional)
+    Private Function PingGateway() As Boolean
+        Dim gateway As String = ToolStripStatusLabel1.Text.Replace("Gateway: ", "").Trim()
+
+        If String.IsNullOrEmpty(gateway) OrElse gateway = "unknown" Then
+            Return False
+        End If
+
+        Try
+            Using ping As New Ping()
+                Dim reply = ping.Send(gateway, 1000)
+                Return reply.Status = IPStatus.Success
+            End Using
+        Catch
+            Return False
+        End Try
+    End Function
+End Class
